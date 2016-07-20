283411a9e7e60736a647d72adea4ccd0cb16428e
kshramt <thisisdummy@example.com>
2016-07-20 17:53:14 +0900

    Support Conda

--- src/PyProj.jl
+++ src/PyProj.jl
@@ -13,7 +13,17 @@ const _transform = PyCall.PyNULL()
 
 
 function __init__()
-    copy!(pyproj, PyCall.pyimport("pyproj"))
+    try
+        copy!(pyproj, PyCall.pyimport("pyproj"))
+    catch e
+        if PyCall.conda
+            info("Installing pyproj by conda")
+            Conda.add("pyproj")
+            copy!(pyproj, PyCall.pyimport("pyproj"))
+        else
+            error("Failed to import pyproj Python package: ", e)
+        end
+    end
 
     copy!(Proj, pyproj[:Proj])
     copy!(Geod, pyproj[:Geod])
