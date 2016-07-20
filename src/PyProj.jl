b8ec505be21c2f2f0b8a8a3d7b7bc4c44c2d0b35
kshramt <thisisdummy@example.com>
2016-07-20 17:12:10 +0900

    Add `Proj`, `Geod` and `transform`

--- src/PyProj.jl
+++ src/PyProj.jl
@@ -1,5 +1,28 @@
+__precompile__()
+
+
 module PyProj
 
-# package code goes here
+import PyCall
+
+
+const pyproj = PyCall.PyNULL()
+const Proj = PyCall.PyNULL()
+const Geod = PyCall.PyNULL()
+const _transform = PyCall.PyNULL()
+
+
+function __init__()
+    copy!(pyproj, PyCall.pyimport("pyproj"))
+
+    copy!(Proj, pyproj[:Proj])
+    copy!(Geod, pyproj[:Geod])
+    copy!(_transform, pyproj[:transform])
+end
+
+
+function transform(p1, p2, x, y, z, radians=false)
+    _transform(p1, p2, x, y, z, radians)
+end
 
 end # module
